aec6cb5d3df2c36fb3a35eb4fa17f0bc9df43319
Alexis Barraza <abarraza24@cnm.edu>
2026-07-14 16:09:53 -0600

    Finished Gatsby Tutorial Part 6

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from  '../components/layout'
 import Seo from '../components/Seo'
 
@@ -9,9 +9,12 @@ const BlogPage = ( { data }) => {
             {
                 data.allMdx.nodes.map((node) =>(
                     <article key={node.id}>
-                        <h2>{node.frontmatter.title}</h2>
+                        <h2>
+                            <Link to={`/blog/${node.frontmatter.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
                         <p>Posted: {node.frontmatter.date}</p>
-                        <p>{node.excerpt}</p>
                     </article>
                 ))
             }
@@ -24,9 +27,9 @@ export const query = graphql `
         allMdx(sort: {frontmatter: { date: DESC}}) {
             nodes {
                 frontmatter {
-                    
                     date(formatString: "MMMM D, YYYY")
                     title
+                    slug
                 }
                 id
                 excerpt
